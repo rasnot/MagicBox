59b9ab0c12d2a3ad75af255b2260c010da772365
bossak <zub.dima2012@gmail.com>
2016-10-13 16:26:54 +0300

    Make changes in models.

--- DataBox/admin.py
+++ DataBox/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from DataBox.models import *
 
 
+class DataSetAdmin(admin.ModelAdmin):
+    list_display = ('id', 'dataSet', 'long_name')
+    #ordering = ['id',]
+
+
 admin.site.register(Dictionary)
 admin.site.register(DictionaryValue)
-admin.site.register(DataSet)
+admin.site.register(DataSet, DataSetAdmin)
 admin.site.register(DataSetAttribute)
 admin.site.register(DataSetValue)
--- DataBox/models.py
+++ DataBox/models.py
@@ -1,9 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.encoding import python_2_unicode_compatible
+import datetime
 
 
-
+@python_2_unicode_compatible  # only if you need to support Python 2
 class Dictionary(models.Model):
     """Модель для роботи із словниками"""
     #pid = models.IntegerField(blank=True, null=True) #parent id, ????
@@ -12,18 +15,19 @@ class Dictionary(models.Model):
     uid = models.IntegerField(blank=True, null=True)
     language_id = models.IntegerField(default=1)
     user_id = models.IntegerField(blank=True, null=True)
-    date_create = models.DateTimeField
+    date_create = models.DateTimeField(default=datetime.datetime.now)
 
     class Meta:
         db_table = 'dictionary'
 
-    def __unicode__(self):
-        return self.name
+    def __str__(self):
+        return '%s %s' % (self.id, self.name)
 
 
+@python_2_unicode_compatible
 class DictionaryValue(models.Model):
     """Модель для роботи із значеннями словників"""
-    dictionary = models.ForeignKey(Dictionary, db_column='dict_id')
+    dictionary = models.ForeignKey(Dictionary, db_column='dict_id', related_name='fk_dictionary_value_dict_id')
     #pid = models.IntegerField(null=True)
     value = models.CharField(max_length=512)
     text_fild1 = models.CharField(max_length=512, blank=True)
@@ -41,71 +45,93 @@ class DictionaryValue(models.Model):
     bool_fild3 = models.NullBooleanField(blank=True)
     uid = models.IntegerField(blank=True, null=True)
     user_id = models.IntegerField(blank=True, null=True)
-    date_create = models.DateTimeField
+    date_create = models.DateTimeField(default=datetime.datetime.now)
 
     class Meta:
         db_table = 'dictionary_value'
 
-    def __unicode__(self):
-        return self.value
+    def __str__(self):
+        return '%s %s' % (self.id, self.value)
 
 
+@python_2_unicode_compatible  # only if you need to support Python 2
 class DataSet(models.Model):
     """Модель для роботи із DataSet-ами та їхніми таблицями"""
-    pid = models.IntegerField(blank=True, null=True)    # parent id, от кого зависит
+    dataSet = models.ForeignKey(
+        'self',
+        blank=True,
+        null=True,
+        db_column='pid',
+        related_name='fk_dataset_pid'
+    )    # parent id, от кого зависит
     long_name = models.CharField(max_length=512)
     short_name = models.CharField(max_length=256, blank=True)
     country_id = models.ForeignKey(
         DictionaryValue,
+        blank=True,
         null=True,
         db_column='dict_country_id',
-        related_name='dict_country_id')    # поле при'язки до країни
+        related_name='fk_dataset_dict_country_id')    # поле при'язки до країни
     region_id = models.ForeignKey(
         DictionaryValue,
+        blank=True,
         null=True,
         db_column='dict_region_id',
-        related_name='dict_region_id')      # поле прив'язки до регіону(обл)
+        related_name='fk_dataset_dict_region_id')      # поле прив'язки до регіону(обл)
+    is_table = models.BooleanField(blank=True, default=False)
     uid = models.IntegerField(blank=True, null=True)        # id таблицы, для реализации языка
     language_id = models.IntegerField(default=1)
     user_id = models.IntegerField(blank=True, null=True)    # кто создал
-    date_create = models.DateTimeField
+    date_create = models.DateTimeField(default=datetime.datetime.now)
 
     class Meta:
         db_table = 'dataset'
 
-    def __unicode__(self):
-        return self.long_name
+    def __str__(self):
+        return '%s %s %s' % (self.id, ('table' if self.is_table else 'null'), self.long_name)
+    #
 
-
+@python_2_unicode_compatible
 class DataSetAttribute(models.Model):
     """Модель для створення атрибутів DataSet"""
     #pid = models.IntegerField(null=True)
-    dataSet = models.ForeignKey(DataSet, db_column='dataset_id')    # id таблицы с данными
-    dictionary = models.ForeignKey(Dictionary, null=True, db_column='dict_id')    # якщо значення береться із довідника
+    dataSet = models.ForeignKey(
+        DataSet,
+        db_column='dataset_id',
+        related_name='fk_dataset_attribute_dataset_id')     # id таблицы с данными
+    dictionary = models.ForeignKey(
+        Dictionary,
+        null=True,
+        db_column='dict_id',
+        related_name='fk_dataset_attribute_dict_id')        # якщо значення береться із довідника
     long_name = models.CharField(max_length=512)
     short_name = models.CharField(max_length=256)
-    column = models.ForeignKey(DictionaryValue, db_column='dict_column_info_id')  # тип колонки char, int, bool...
+    column = models.ForeignKey(
+        DictionaryValue,
+        db_column='dict_column_info_id',
+        related_name='fk_dataset_attribute_dict_column_info_id')  # тип колонки char, int, bool...
     uid = models.IntegerField(blank=True, null=True)
     user_id = models.IntegerField(blank=True, null=True)    # в случае изменения
-    date_create = models.DateTimeField
+    date_create = models.DateTimeField(default=datetime.datetime.now)
 
     class Meta:
         db_table = 'dataset_attribute'
 
-    def __unicode__(self):
-        return self.dataSet.long_name
+    def __str__(self):
+        return '%s %s' % (self.id, self.long_name)
 
 
+@python_2_unicode_compatible
 class DataSetValue(models.Model):
     """Модель для роботи із значеннями DataSet"""
     dataSet = models.ForeignKey(DataSet, db_column='dataset_id')
     value = models.TextField()
     uid = models.IntegerField(blank=True, null=True)
     user_id = models.IntegerField(blank=True, null=True)
-    date_create = models.DateTimeField
+    date_create = models.DateTimeField(default=datetime.datetime.now)
 
     class Meta:
         db_table = 'dataset_value'
 
-    def __unicode__(self):
-        return self.dataSet.long_name
+    def __str__(self):
+        return '%s' % self.id
